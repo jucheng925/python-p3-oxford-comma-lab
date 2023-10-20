591b68c75637c66f31a2ff5f7936db8999a9db8f
Julie Cheng <jucheng925@gmail.com>
2023-10-20 01:28:57 -0400

    oxford comma function complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,6 @@
 def oxford_comma(items):
-    return None
+    if len(items) >= 2:
+        items[-1] = f'and {items[-1]}'
+        return " ".join(items) if len(items) == 2 else ", ".join(items)
+    else:
+        return str(items[0])
